4b133eda204a19a13397712e417de56c2e23f25a
Ibrahim <ichibsah@outlook.com>
2017-09-01 07:10:05 +0200

    01/09/2017  7:10:04.96

--- github-push.bat
+++ github-push.bat
@@ -2,7 +2,7 @@ git add .
 
 git commit -m "%date% %time%"
 
-REM git push http://isawadogo@bitbucket.nws-intern.de/scm/~isawadogo/sitemap-tool.git --force
+git push https://github.com/ichibsah/smartapi.git --force
 
 git push https://github.com/ichibsah/smartapi.git
 
